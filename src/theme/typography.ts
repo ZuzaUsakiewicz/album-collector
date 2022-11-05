00bff373d9f889de3adadf08d3a6e751a5be3449
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-11-05 21:03:51 +0100

    style: add typography size and weight variables

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,4 +1,17 @@
 export const typography = {
-  smallFont: "1rem",
-  largeFont: "3rem",
+  size: {
+    xsFont: "0.875rem",
+    smFont: "1rem",
+    mdFont: "1.25rem",
+    regFont: "1.5rem",
+    lgFont: "1.875rem",
+    xlFont: "2rem",
+    xxlFont: "2.5rem",
+  },
+  weight: {
+    light: "300",
+    regular: "400",
+    medium: "500",
+    bold: "700",
+  },
 };
